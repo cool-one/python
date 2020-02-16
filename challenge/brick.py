dcaa6594f84f20c9f9426cc85c0d8b6f380c811e
rx <coolwatertx@gmail.com>
2020-02-16 12:57:32 -0600

    formatting

--- challenge/brick.py
+++ challenge/brick.py
@@ -1,6 +1,8 @@
   
 """
-We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and big bricks (5 inches each). Return True if it is possible to make the goal by choosing from the given bricks. This is a little harder than it looks and can be done without any loops.
+We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and 
+big bricks (5 inches each). Return True if it is possible to make the goal by choosing from the given bricks. 
+This is a little harder than it looks and can be done without any loops.
 """
 def make_bricks(small, big, goal):
     # The bare minimum of small bricks for any condition to be eligible to pass.
